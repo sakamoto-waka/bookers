3c47844bc3616abfc2f53531ee772a4f2d948c1d
sakamoto-waka <wakanmhl@gmail.com>
2022-02-26 13:13:32 +0000

    [add] index table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end  
 
   def show
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:books).permit(:title, :body)
+  end  
+  
 end
