50c2e3199950c6f1e2d00628ad709ceca275a515
sakamoto-waka <wakanmhl@gmail.com>
2022-02-27 02:44:35 +0000

    [update] cording done ready for test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
     end
   end
 
